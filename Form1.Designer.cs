f6171618735f410159ad5a58710d6f0180dcf70d
LaercioCosta <laerciocostam1999@gmail.com>
2021-08-23 09:32:10 -0300

    Sistema Cantina

---
+++ Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace Sistema_Cantina
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.lstCaixa = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.picImagem = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnApagar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(12, 98);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(210, 23);
+            this.txtCodigo.TabIndex = 0;
+            this.txtCodigo.TextChanged += new System.EventHandler(this.txtCodigo_TextChanged);
+            // 
+            // lstCaixa
+            // 
+            this.lstCaixa.FormattingEnabled = true;
+            this.lstCaixa.ItemHeight = 15;
+            this.lstCaixa.Location = new System.Drawing.Point(228, 75);
+            this.lstCaixa.Name = "lstCaixa";
+            this.lstCaixa.Size = new System.Drawing.Size(202, 259);
+            this.lstCaixa.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(175, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Sistema Cantina";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(45, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Código do Produto";
+            // 
+            // picImagem
+            // 
+            this.picImagem.Location = new System.Drawing.Point(12, 136);
+            this.picImagem.Name = "picImagem";
+            this.picImagem.Size = new System.Drawing.Size(210, 198);
+            this.picImagem.TabIndex = 4;
+            this.picImagem.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 357);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Valor total:";
+            // 
+            // btnApagar
+            // 
+            this.btnApagar.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnApagar.Location = new System.Drawing.Point(228, 343);
+            this.btnApagar.Name = "btnApagar";
+            this.btnApagar.Size = new System.Drawing.Size(202, 50);
+            this.btnApagar.TabIndex = 6;
+            this.btnApagar.Text = "Apagar";
+            this.btnApagar.UseVisualStyleBackColor = true;
+            this.btnApagar.Click += new System.EventHandler(this.btnApagar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 402);
+            this.Controls.Add(this.btnApagar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.picImagem);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstCaixa);
+            this.Controls.Add(this.txtCodigo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.ListBox lstCaixa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox picImagem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnApagar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_Cantina
+{
+    public partial class Form1 : Form
+    {
+        string[] produto = new string[10];
+        string[] codigo = new string[10];
+        double[] valor = new double[10];
+        double soma = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtCodigo_TextChanged(object sender, EventArgs e)
+        {
+            if(txtCodigo.Text.Length == 3)
+            {
+                int indice = 0;
+                for(int prod = 1;prod < codigo.Length; prod++)
+                {
+                    if(txtCodigo.Text == codigo[prod])
+                    {
+                        indice = prod;
+                    }
+                }
+                if(indice == 0)
+                {
+                    MessageBox.Show("Produto não encontrado");
+                }
+                else
+                {
+                    lstCaixa.Items.Add(txtCodigo.Text + " -- " + produto[indice] + " -- RS:" + valor[indice]);
+
+                    soma = soma + valor[indice];
+                    label3.Text = ("Valor Total R$: " + soma);
+                    picImagem.ImageLocation = "G:/Estudo/Fundação Bradesco/Sistema Cantina/img/" + codigo[indice] + ".jpg";
+                    txtCodigo.Text = "";
+                    txtCodigo.Focus();
+
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            carregarArray();
+            soma = 0;
+        }
+        private void carregarArray()
+        {
+            codigo[1] = "001";
+            codigo[2] = "002";
+            codigo[3] = "003";
+            codigo[4] = "004";
+            codigo[5] = "005";
+
+            produto[1] = "Pastel";
+            produto[2] = "Coxinha";
+            produto[3] = "Refrigerante";
+            produto[4] = "Hot-dog";
+            produto[5] = "Suco";
+
+            valor[1] = 2.50;
+            valor[2] = 2.50;
+            valor[3] = 5.00;
+            valor[4] = 3.00;
+            valor[5] = 2.50;
+        }
+
+        private void btnApagar_Click(object sender, EventArgs e)
+        {
+            lstCaixa.Items.Clear();
+            soma = 0;
+            label3.Text = "Valor Total R$: ";
+            txtCodigo.Focus();
+        }
+    }
+}
